3a2bcfba9c61ab5be595ed3f556ec779cde8e1de
Henry Kay <mireyhen@gmail.com>
2025-07-10 08:42:30 +0300

    Implement book rendering logic with renderBooksToTable()

--- java.js
+++ java.js
@@ -1,13 +1,15 @@
+//-----CORE LOGIC OF CREATE A BOOK AND DISPLAY-------//
 const myLibrary = [];
 
-function Book({ title, author, read, id }) {
+function Book({ title, author, read, pages, id }) {
   this.title = title;
   this.author = author;
+  this.pages = pages;
   this.read = read;
   this.id = crypto.randomUUID();
 }
-function addBookToLibrary({ title, author, read }) {
-  const book = new Book({ title, author, read });
+function addBookToLibrary({ title, author, read, pages }) {
+  const book = new Book({ title, author, read, pages });
   myLibrary.push(book);
   return myLibrary;
 }
@@ -16,6 +18,29 @@ function logBooks(myArray) {
 }
 function renderBooks(array) {
   return array.map(
-    (book) => `${book.title} by ${book.author} - Read: ${book.read}`
+    (book) =>
+      `${book.title} by ${book.author} with ${book.pages} pages - Read: ${book.read}`
   );
+}
+
+///--------UI-setup--------//
+
+//rendering books to ui
+function renderBooksToTable(arr) {
+  const tableBody = document.getElementById("book-table-only");
+  tableBody.innerHTML = "";
+  for (let i = 0; i < arr.length; i++) {
+    let currentBook = arr[i];
+    const tableRow = document.createElement("tr");
+    tableBody.appendChild(tableRow);
+    const cell1 = document.createElement("td");
+    cell1.textContent = currentBook.title;
+    tableRow.appendChild(cell1);
+    const cell2 = document.createElement("td");
+    cell2.textContent = currentBook.author;
+    tableRow.appendChild(cell2);
+    const cell3 = document.createElement("td");
+    cell3.textContent = currentBook.read;
+    tableRow.appendChild(cell3);
+  }
 }
